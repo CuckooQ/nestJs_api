fc821ec9b9d8d416c59992f8a3c5d702d245dd9f
jaeyong4536 <jaeyong4536@gmail.com>
2021-01-24 00:13:26 +0900

    appController & appService

--- src/movies/dto/update-video.dto.ts
+++ src/movies/dto/update-video.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsString } from "class-validator";
 import { CreateMovieDto } from "./create-video.dto";
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
